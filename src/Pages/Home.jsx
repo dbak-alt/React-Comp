0cf254bc23372649252810781e30dc1aaf6af7cf
Dbak <deebakk501@gmail.com>
2024-10-04 22:18:38 +0530

    updated assets

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/home.css';
-import arrow from '../../assets/arrowright.png';
+import arrow from '../../src/assets/arrowright.png';
 
 function Home() {
     return (
